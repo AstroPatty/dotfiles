347df04f6b7505aa9fd94da6ac0f0704a90dbc2d
Patrick Wells <patrick.wells.95@gmail.com>
2024-09-08 17:12:54 -0500

    add nvim

---
+++ nvim/.config/nvim/lua/plugins/markprev.lua
@@ -0,0 +1,12 @@
+local config = -- install without yarn or npm
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  }
+-- install with yarn or npm
+
+return config
---
+++ nvim/.config/nvim/lua/plugins/treesitter_textobj.lua
@@ -0,0 +1,5 @@
+local cfg = {
+  'nvim-treesitter/nvim-treesitter-textobjects',
+}
+
+return cfg
